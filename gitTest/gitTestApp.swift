cdf0b85b4255c08b881ae8cdc21acacd4df2e096
razanalsunaidi <razanalsunedi@gmail.com>
2024-09-26 17:05:32 +0300

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Razan Alsunaidi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
